6bfc1a1a14737f20719501c359beeafeff4d5e26
tok3 <tok3@users.noreply.github.com>
2024-08-14 11:47:18 +0000

    Fix styling

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,8 +1,10 @@
 <?php
+
 use Tok3\WeightConversions\Weight;
+
 it('can convert kilograms to pounds correctly', function () {
 
-    $pounds =  Weight::kilograms(100)->toPounds();
+    $pounds = Weight::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
 });
