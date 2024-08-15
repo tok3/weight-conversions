123042f42cfd05dc1f30d6542c13b666bb0130b5
tok3 <tobias@eq3w.de>
2024-08-15 13:25:33 +0200

    weight class completed

--- src/Weight.php
+++ src/Weight.php
@@ -2,4 +2,27 @@
 
 namespace Tok3\WeightConversions;
 
-class Weight {}
+class Weight
+{
+    protected float $kilograms;
+
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toPounds(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
+
+    public function toStones(): float
+    {
+        return $this->kilograms * 0.15747;
+    }
+}
