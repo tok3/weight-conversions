f8c571481123a1f2f1d09e4f400a7fc1a8e37776
tok3 <tobias@eq3w.de>
2024-08-16 17:27:44 +0200

    codestyle test

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -3,8 +3,13 @@
 use Tok3\WeightConversions\Weight;
 
 it('can convert kilograms to pounds correctly', function () {
-
     $pounds = Weight::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
+});
+
+it('can convert kilograms to stones correctly', function () {
+    $stones = Weight::kilograms(100)->toStones();
+
+    expect($stones)->toEqual(15.747);
 });
